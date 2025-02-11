d1ca9d74ba57f73d6bbf1b97aca5d02bc3a25c55
AnoniM61 <anonimlox58@gmail.com>
2025-02-11 22:13:12 +0300

    Add vader

--- my_proj/my_proj/nltk_analyzer/nltk_downloder.py
+++ my_proj/my_proj/nltk_analyzer/nltk_downloder.py
@@ -1,2 +1,6 @@
 import nltk
-from dependency_injector
+from nltk import word_tokenize
+from nltk.corpus import opinion_lexicon
+import vader
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from vader_multi import vaderMulti
